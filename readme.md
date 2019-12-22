bc709fe2ff9ea4f2450a580c677dd90ed768d015
Tiago Henrique Iwamoto <tiago.iwamoto@gmail.com>
2019-12-22 10:19:12 -0300

    Add the save, update, queryRaw and select a initial readme.md

---
+++ readme.md
@@ -0,0 +1,26 @@
+# Spring-data Helper
+    What this helper is ?
+        a service to ...
+        
+# Tecnologies
+    Java 1.8
+    Maven 3.6.x
+    SOLID && KISS
+
+# Build
+    mvn clean install
+    mvn -DskipTests clean install
+    
+# Add in your project
+    <dependency>
+        <groupId>work.iwacloud</groupId>
+        <artifactId>springdata-helper</artifactId>
+        <version>1.0</version>
+    </dependency>
+
+    
+# Main developers
+    Tiago Henrique Iwamoto - https://www.linkedin.com/in/tiago-iwamoto/
+    
+# Any help ?
+    Members contacts
--- src/main/java/work/iwacloud/springdatahelper/repositories/IwaRepository.java
+++ src/main/java/work/iwacloud/springdatahelper/repositories/IwaRepository.java
@@ -8,8 +8,13 @@ package work.iwacloud.springdatahelper.repositories;
  * 22/12/2019 - 00:31
  */
 
+import org.json.JSONArray;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataAccessException;
 import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.ResultSetExtractor;
+import org.springframework.stereotype.Component;
+import work.iwacloud.springdatahelper.exception.IwaException;
 import work.iwacloud.springdatahelper.helper.TableHelper;
 import work.iwacloud.springdatahelper.objects.DataTransfer;
 import work.iwacloud.springdatahelper.objects.IwaTable;
@@ -18,12 +23,16 @@ import work.iwacloud.springdatahelper.objects.StatusOperation;
 import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.Query;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
 import java.util.LinkedList;
 
 @SuppressWarnings("all")
+@Component
 public class IwaRepository {
 
-    private static final Long TIME_TO_EXECUTE = 2000L;
+    private static final Long TIME_TO_EXECUTE = 1000L;
     private EntityManagerFactory entityManagerFactory;
     private JdbcTemplate jdbcTemplate;
 
@@ -31,6 +40,41 @@ public class IwaRepository {
     public IwaRepository(JdbcTemplate jdbcTemplate, EntityManagerFactory entityManagerFactory) {
         this.jdbcTemplate = jdbcTemplate;
         this.entityManagerFactory = entityManagerFactory;
+    }
+
+    /**
+     * Method configured to return a json array,
+     * in this context i have to set a Object.
+     * @param select
+     * @return
+     */
+    public Object select(String select, Boolean asList){
+
+        if(select.startsWith("select") || select.startsWith("SELECT")){
+            return jdbcTemplate.query(select, new ResultSetExtractor<Object>() {
+
+                @Override
+                public Object extractData(ResultSet resultSet) throws SQLException, DataAccessException {
+                    try {
+                        return new TableHelper().serializeResultSet(resultSet);
+                    } catch (IwaException e) {
+                        if(asList){
+                            return new ArrayList<>();
+                        }else{
+                            return new JSONArray();
+                        }
+                    }
+                }
+
+            });
+        }else{
+            if(asList){
+                return new ArrayList<>();
+            }else{
+                return new JSONArray();
+            }
+        }
+
     }
 
     /**
@@ -64,6 +108,7 @@ public class IwaRepository {
                 query.setParameter(position++, value.getValue());
             }
             em.getTransaction().begin();
+            /* Try to execute 3 times */
             while(timesExecuted < 3){
                 Integer queryResult = query.executeUpdate();
                 if(queryResult > 0){
@@ -77,7 +122,98 @@ public class IwaRepository {
             return new DataTransfer(StatusOperation.ABORTED, "");
 
         }catch (Exception e){
-            //new ProcessingException().tratarException(e, getClass().getSimpleName());
+            return new DataTransfer(StatusOperation.ERROR, "");
+        }finally {
+            if(em.isOpen()){
+                em.clear();
+                em.close();
+            }
+        }
+    }
+
+    /**
+     * Update a line on table of database
+     * using spring-data entitymanager
+     * @param tableColumns
+     * @param table
+     * @param where
+     * @return
+     */
+    public DataTransfer<StatusOperation, Object> update(LinkedList<IwaTable> tableColumns, String table, IwaTable where){
+        String columns = "";
+        int position = 1;
+        int timesExecuted = 0;
+
+        for(IwaTable value : tableColumns){
+            if(position == tableColumns.size()){
+                columns += value.getColumn() + " = ?";
+            }else{
+                columns += value.getColumn() + " = ? , ";
+            }
+            ++position;
+        }
+
+        EntityManager em = entityManagerFactory.createEntityManager();
+
+        try{
+            String whereToUpdate = String.format("%s = %s", where.getColumn(), where.getValue());
+
+            String sqlCommand = String.format("UPDATE %s SET %s WHERE %s", table, columns, whereToUpdate);
+            Query query = em.createNativeQuery(sqlCommand);
+            position = 1;
+            for (IwaTable value : tableColumns) {
+                query.setParameter(position++, value.getValue());
+            }
+            em.getTransaction().begin();
+            while (timesExecuted < 3){
+                Integer queryResult = query.executeUpdate();
+                if(queryResult > 0){
+                    em.getTransaction().commit();
+                    return new DataTransfer<>(StatusOperation.EXECUTED, "");
+                }else{
+                    Thread.sleep(TIME_TO_EXECUTE);
+                    timesExecuted++;
+                }
+            }
+
+            return new DataTransfer(StatusOperation.ABORTED, "");
+        }catch (Exception e){
+            return new DataTransfer(StatusOperation.ERROR, "");
+        }finally {
+            if(em.isOpen()){
+                em.clear();
+                em.close();
+            }
+        }
+    }
+
+    /**
+     * execute a pure sql command
+     * using spring-data entitymanager
+     * @param command
+     * @return
+     */
+    public DataTransfer<StatusOperation, Object> rawQuery(String command){
+        EntityManager em = entityManagerFactory.createEntityManager();
+
+        try{
+            int timesExecuted = 0;
+            String sqlCommand = String.format(command);
+            Query query = em.createNativeQuery(sqlCommand);
+            em.getTransaction().begin();
+            /* Try to execute 3 times */
+            while(timesExecuted < 3) {
+                Integer queryResult = query.executeUpdate();
+                if (queryResult > 0) {
+                    em.getTransaction().commit();
+                    return new DataTransfer(StatusOperation.EXECUTED, "");
+                } else {
+                    Thread.sleep(TIME_TO_EXECUTE);
+                    timesExecuted++;
+                }
+            }
+            return new DataTransfer(StatusOperation.ABORTED, "");
+        }catch (Exception e){
             return new DataTransfer(StatusOperation.ERROR, "");
         }finally {
             if(em.isOpen()){
